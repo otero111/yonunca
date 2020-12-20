4fb7bddd70c70207fb2b97a2a0753555ae9320c6
Ricardo Otero <63835103+otero111@users.noreply.github.com>
2020-12-20 13:13:33 -0500

    Create codigo.js

---
+++ codigo.js
@@ -0,0 +1,121 @@
+	alert("Instrucciones: Debe de tomar un trago cuando usted si hizo lo que le dice el Yo nunca. La aplicacion esta pensada para todo tipo de publico pero esta escrita con el vocabulario usado en Cuba. La aplicacion aun es joven y necesita de su ayuda para crecer. Envie nuevas preguntas al correo rop010698@gmail.com.Un placer ayudarle a pecar. ")
+		
+		var textos = new Array()
+		textos[0] = " Yo nunca he vuelto con mi ex.";
+		textos[1] = " He mandado nudes.";
+		textos[2] = " He aparentado no  estar borracho para no discutir con mi madre.";
+	    textos[3] = " Yo nunca me he liado con el ex de un amig@";
+		textos[4] = "  he escrito a mi ex una noche de fiesta";
+		textos[5] = "  he fingido que me llamaban para escapar de una cita o lugar";
+		textos[6] = " he estado más de tres días sin ducharme ";
+		textos[7] = " me he imaginado siendo del sexo opuesto ";
+		textos[8] = "  he  perdido un movil";
+		textos[9] = "  mentí en el Yo Nunca";
+		textos[10] = "  he estornudado y me ha salido un moco";
+		textos[11] = "  he indicado mal un camino cuando me han preguntado";
+		textos[12] = "  he olido mi propio pedo";
+		textos[13] = "  he intentado mover cosas con la mente (sin resultado)";
+		textos[14] = "  he mirado el papel después de ir al baño";
+		textos[15] = " he mandado un audio subidito de tono a mi madre por error"; 
+		textos[16] = " he amanecido en un parque borracho"; 
+		textos[17] = "He salido con ropa interior rota"; 
+		textos[18] = "He pasado pena al acostarme con alguien desconocid@"; 
+		textos[19] = "Me fugue de la escuela"; 
+		textos[20] = "Hice fraude"; 
+		textos[21] = "He tratado de entrar al Facebook de alguien"; 
+		textos[22] = "He visto desnud@ por error a alguien"; 
+		textos[23] = "No me he confundido de contacto en WhatSapp y le he dicho algo ofencivo"; 
+		textos[24] = "He visto porno"; 
+		textos[25] = "He sido infiel"; 
+		textos[26] = " He pensado que nadie me quiere"; 
+		textos[27] = "He intentado suicidarme"; 
+		textos[28] = "Nunca he mirado escondido a alguien mientras se cambiaba"; 
+		textos[29] = "He pasado años atras de una persona y no le dije que me gustaba"; 
+		textos[30] = "He hecho un baile sexy para alguien en las redes"; 
+		textos[31] = "He he sentido atraid@ por alguien con quien chateo en las redes"; 
+		textos[32] = "He estado detenido"; 
+		textos[33] = "Me he mojado la punta de los labios para aparentar que estoy tomando porque estoy borracho"; 
+		textos[34] = "He sido estafado"; 
+		textos[35] = "He publicado por equivocacion una foto comprometedora"; 
+		textos[36] = "He dicho que voy a casa de un@ amig@ para escaparme de fiestas"; 
+		textos[37] = "Me he ido sin pagar de un lugar (por equivocacion o a proposito)"; 
+		textos[38] = "He visto a dos amigos mios teniendo sexo"; 
+		textos[39] = "He follado en publico"; 
+		textos[40] = "He usado ropa del sexo opuesto"; 
+		textos[41] = "He ido a salir con un piquete y he regresado con otro"; 
+		textos[42] = "Me he juntado con un piquete que ni conozco solo para emborracharme y pasar la noche"; 
+		textos[43] = "Me he masturbado en una casa agena"; 
+		textos[44] = "He ido caminando del Vedado o La Habana hasta mi casa"; 
+		textos[45] = "He sido asaltad@"; 
+		textos[46] = "Me he tocado el celular despues de tirarme un pedo para aparentar"; 
+		textos[47] = "Me mirado hacia atras despues de tirarme un pedo"; 
+		textos[48] = "He cagado en la calle"; 
+		textos[49] = "Me he acostado con una persona mucho mayor que yo (+ de 20 años)"; 
+		textos[50] = "He visto porno en la escuela"; 
+		textos[51] = "Yo nunca he aparentado estar tomando para que no vean que estoy borracho"; 
+		textos[52] = "He sateado con mas de 2 personas a la vez por mensajes"; 
+		textos[53] = "He mentido por amor "; 
+		textos[54] = "Tuve  una fantasía rara"; 
+		textos[55] = "He traicionado a un amigo para beneficiarme"; 
+		textos[56] = "Me he fijado en alguien que no debo"; 
+		textos[57] = "He tenido un orgasmo"; 
+		textos[58] = "He follado más de 3 veces seguidas "; 
+		textos[59] = "He estado con más de un@ chic@ en un día"; 
+		textos[60] = "He hecho una orgía"; 
+		textos[61] = "He tenido sexo oral"; 
+		textos[62] = "He tenido sexo anal"; 
+		textos[63] = "Le he deseado la muerte alguien"; 
+		textos[64] = "He sentido envidia"; 
+		textos[65] = "Me he alegrado de la desgracia de alguien"; 
+		textos[66] = "He fingido un orgasmo "; 
+		textos[67] = "He mentido a mi madre"; 
+		textos[68] = "He deseado irme lejos por un tiempo"; 
+		textos[69] = "He tenido ganas de morirme"; 
+		textos[70] = "Me he sentido sol@"; 
+		textos[71] = "He dudado de mi"; 
+		textos[72] = "He fantaseado con alguien mientras converso con esa persona"; 
+		textos[73] = "He desconfiado de mis amistades"; 
+		textos[74] = "He sentido vergüenza de mi cuerpo "; 
+		textos[75] = "He tenido complejos"; 
+		textos[76] = "Me he enamorado"; 
+		textos[77] = "Me he fijado en otra persona q no sea mi pareja "; 
+		textos[78] = "Me he sentido atraída por alguien del sexo opuesto"; 
+		textos[79] = "Me he sentido atraída por alguien del mismo sexo"; 
+		textos[80] = "He fingido estar borracho"; 
+		textos[81] = "He dejado tirado a alguien que me necesita"; 
+		textos[82] = "He llegado tarde a una cita"; 
+		textos[83] = "He tenido una experiencia sexual interesante"; 
+		textos[84] = "He follado delante de alguien"; 
+		textos[85] = "He follado con un familiar"; 
+		textos[86] = "Me he enamorado de un familiar"; 
+		textos[87] = "He tenido sexo en la escuela"; 
+		textos[88] = "He sido humillado en publico"; 
+		textos[89] = "Me he caido de la silla"; 
+		textos[90] = "Me he caido en publico"; 
+		textos[91] = "He llorado en publico"; 
+		textos[92] = "Me he exitado pensando en cosas pervertidas en un luagr publico"; 
+		textos[93] = "Le he hecho bromas pesadas a desconocidos"; 
+        textos[94] = "He sentido curiosidad sobre que se siente estar con alguien de tu propio sexo"; 
+        textos[95] = "Me he orinado mientras dormia despues de grande"; 
+		textos[96] = "He visto porno gay"; 
+		textos[97] = "He vendido algo ilegal"; 
+		textos[98] = "Me he drogado"; 
+		textos[99] = "He inventado excusas para no follar"; 
+		textos[100] = "He matado a un familiar en las mentiras a los profesores"; 
+		textos[101]="Le dije compañera a un compañero sin querer";
+		textos[102]="He sido estafado";
+	
+
+
+        function aleatorio() {
+          document.getElementById("resultado").innerHTML = "";
+          if (textos.length > 0) {
+    nro = Math.floor(Math.random() * (textos.length - 0) + 0);
+             document.getElementById("resultado").innerHTML = textos[nro];
+    //delete this index from the array so that it does not repeat
+             textos.splice(nro, 1);
+  } else {	
+
+            document.getElementById("resultado").innerHTML = "Se acabo 😥, puede contribuir con mas preguntas enviando un correo a rop010698@gmail.com <br><br> <a class='ws' href='https://api.whatsapp.com/send?phone=+5358380944&text=Hola!%20Quiero%20cooperar%20con%20YoNuncaCuban!'>Contacte al desarrollador si desea cooperar</a> <br><br> Proximamente planes premium";
+  }
+}
